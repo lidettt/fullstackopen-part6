d4f17505a78e6cbd2dacbe76251b4ea551a96df8
Lidet Voeun <lidetvoeun@gmail.com>
2026-08-12 22:50:14 +0700

    Exercise 6.17

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createAnecdote } from "../requests";
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
+  });
+
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.reset()
-    console.log('new anecdote')
-  }
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.reset();
+    newAnecdoteMutation.mutate({ content, votes: 0 });
+  };
 
   return (
     <div>
@@ -14,7 +26,7 @@ const AnecdoteForm = () => {
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
