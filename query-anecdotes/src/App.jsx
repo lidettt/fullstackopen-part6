00f861ce8df209af452d9c16fc0d8d28896cb259
Lidet Voeun <lidetvoeun@gmail.com>
2026-08-12 23:00:48 +0700

    Exercise 6.18

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,11 +1,20 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
-import { getAnecdotes } from "./requests";
-import { useQuery } from "@tanstack/react-query";
+import { getAnecdotes, updateAnecdote } from "./requests";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 const App = () => {
+  const queryClient = useQueryClient();
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
+  });
+
   const handleVote = (anecdote) => {
-    console.log("vote");
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
   };
 
   const result = useQuery({
